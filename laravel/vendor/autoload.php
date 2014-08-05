384f0c905f3a3474c23992fa855a68c871262b19
carlos.juarez <t7marketing004@gmail.com>
2014-08-05 17:43:22 -0500

    marketing publicar contenido de cliente

--- laravel/vendor/autoload.php
+++ laravel/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit9d3e250d7ce87e7cba64891620a9b00c::getLoader();
+return ComposerAutoloaderInitcb76f4e52e07c015d898a1d2ef2b0aff::getLoader();
