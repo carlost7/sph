39c72f3f6205dca182b24141bc1988902c93297b
carlos.juarez <t7marketing004@gmail.com>
2014-09-03 11:30:39 -0500

    cambio de carpetas

--- laravel/bootstrap/paths.php
+++ laravel/bootstrap/paths.php
@@ -23,7 +23,7 @@ return array(
       | point for web requests into these applications from the outside.
       |
      */
-    'public' => __DIR__ . '/../public/',
+    'public' => __DIR__ . '/../public_html/sphellar/',
     /*
       |--------------------------------------------------------------------------
       | Base Path
