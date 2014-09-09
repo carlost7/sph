49883d1b4d4a147f486c28f24880b9710b18d5e5
Carlos Juarez <carlos.juarez@t7marketing.com>
2014-09-09 00:36:53 -0500

    show negocios, eventos

--- laravel/app/views/contenido/index.blade.php
+++ laravel/app/views/contenido/index.blade.php
@@ -5,32 +5,62 @@
       @include('layouts.show_catalog')
 
       <div class="row">
-            @foreach($negocios as $negocio)
-            <div class="col-sm-4">
-                  @if(count($negocio->imagen))
-                  <img src="{{Config::get('params.path_public_image').$negocio->imagen->path.$negocio->imagen->nombre}}" alt="{{ $negocio->imagen->alt }}" />
-                  @endif
-                  <h2>{{ $negocio->nombre }}</h2>
-                  <hr />
-                  <p>Subcategoria: {{ $negocio->subcategoria->subcategoria }}</p>
-                  <hr />                  
-                  <p>Zona: {{ $negocio->zona->zona }}</p>
-                  <p>Dirección: {{ $negocio->direccion }}</p>
-                  <p>Teléfono: {{ $negocio->telefono }}</p>
-                  @if(count($negocio->especial))
-                  <p>Web: <a href="{{$negocio->especial->webpage}}">{{$negocio->nombre}}</a></p>
-                  @endif
-                  
-                  
+            <div class="col-sm-8">
+                  @foreach($negocios as $negocio)
+                  <div class="col-sm-4">
+                        @if(count($negocio->imagen))
+                        <img src="{{Config::get('params.path_public_image').$negocio->imagen->path.$negocio->imagen->nombre}}" alt="{{ $negocio->imagen->alt }}" />
+                        @endif
+                        <h2>{{ HTML::linkRoute('negocios.show',$negocio->nombre,$negocio->id) }}</h2>
+                        <hr />
+                        <p>Subcategoria: {{ $negocio->subcategoria->subcategoria }}</p>
+                        <hr />                  
+                        <p>Zona: {{ $negocio->zona->zona }}</p>
+                        <p>Dirección: {{ $negocio->direccion }}</p>
+                        <p>Teléfono: {{ $negocio->telefono }}</p>
+                        @if(count($negocio->especial))
+                        <p>Web: <a href="{{$negocio->especial->webpage}}">{{$negocio->nombre}}</a></p>
+                        @endif
+
+
+                  </div>
+                  @endforeach                  
+
             </div>
-            @endforeach
+            <div class="col-sm-4">
+                  @foreach($eventos as $evento)
+                  <div class="col-sm-4">
+                        @if(count($evento->imagen))
+                        <img src="{{Config::get('params.path_public_image').$evento->imagen->path.$evento->imagen->nombre}}" alt="{{ $evento->imagen->alt }}" />
+                        @endif
+                        <h2>{{HTML::linkRoute('eventos.show',$evento->nombre,$evento->id) }}</h2>
+                        <hr />
+                        <p>Subcategoria: {{ $evento->subcategoria->subcategoria }}</p>
+                        <hr />                  
+                        <p>Zona: {{ $evento->zona->zona }}</p>
+                        <p>Dirección: {{ $evento->direccion }}</p>
+                        @if(count($negocio->especial))
+                        <p>Web: <a href="{{$evento->especial->web}}">{{$evento->nombre}}</a></p>
+                        @endif
+
+
+                  </div>
+                  @endforeach                  
+            </div>
       </div>
       <div class="row">
-            <div class="col-sm-12">
+            <div class="col-sm-8">
                   <div class="text-center">
                         {{ $negocios->links()}}      
                   </div>                  
+            </div>    
+
+            <div class="col-sm-4">
+                  <div class="text-center">
+                        {{ $eventos->links()}}      
+                  </div>                  
             </div>            
+
       </div>
 
 
