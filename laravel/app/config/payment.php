1f377d2829429b2c3654f20a80adca337c727706
Carlos Juarez <carlos.juarez@t7marketing.com>
2014-09-15 00:45:48 -0500

    payment deactivated

--- laravel/app/config/payment.php
+++ laravel/app/config/payment.php
@@ -9,5 +9,5 @@ return array(
     'client_id' => '383116726755305',
     'client_socket' => 'yLijSYs58fG1A1D1r8DPPGrggSlgl94z',
     'init_point' => 'sandbox_init_point',
-    'sandbox_mode' => false,
+    'sandbox_mode' => true,
 );
