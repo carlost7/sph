ab8f9fd00fc99f52f85a23cba57079023c7be9a6
carlos.juarez <t7marketing004@gmail.com>
2014-09-17 12:40:10 -0500

    buscador js

--- public_html/guia/js/main.js
+++ public_html/guia/js/main.js
@@ -2,14 +2,14 @@
  * Autocompletadores de texto
  */
 
-$("#local").autocomplete({
+jQuery("#local").autocomplete({
     serviceUrl: base_url + "/catalogo_zonas",
     onSelect: function(suggestion) {
         jQuery("#tipolocal").val(suggestion.data);
     }
 });
 
-$("#cat").autocomplete({
+jQuery("#cat").autocomplete({
     serviceUrl: base_url + "/catalogo_categorias",
     onSelect: function(suggestion) {
         jQuery("#tipocat").val(suggestion.data);
