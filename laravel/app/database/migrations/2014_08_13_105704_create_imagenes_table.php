469caab5cff7ea6827f73f4640f2f48e1380a034
carlos.juarez <t7marketing004@gmail.com>
2014-09-19 14:25:57 -0500

    add user page and change image directory

--- laravel/app/database/migrations/2014_08_13_105704_create_imagenes_table.php
+++ laravel/app/database/migrations/2014_08_13_105704_create_imagenes_table.php
@@ -22,8 +22,8 @@ class CreateImagenesTable extends Migration
                   $table->integer('imageable_id');
                   $table->string('alt');
                   $table->timestamps();
-                  $table->integer('cliente_id')->unsigned();
-                  $table->foreign('cliente_id')->references('id')->on('clientes')->onDelete('cascade')->onUpdate('cascade');
+                  $table->integer('user_id')->unsigned();
+                  $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');                  
             });
       }
 
