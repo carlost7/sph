dc8134724d3bba952f74f05f832ff8fc74168023
carlos.juarez <t7marketing004@gmail.com>
2014-10-16 16:54:45 -0500

    config session

--- laravel/app/config/session.php
+++ laravel/app/config/session.php
@@ -104,7 +104,7 @@ return array(
       | available to in your application. A sensible default has been set.
       |
      */
-    'domain' => "sphellar.com/mx/guia",
+    'domain' => "http://sphellar.com/mx/guia",
     /*
       |--------------------------------------------------------------------------
       | HTTPS Only Cookies
