ec79c3a355fff02f8d8c46e8edddd9586cb2be55
carlos <carlos@carlos>
2015-01-23 15:57:03 -0600

    Modified all controllers to work with ardent

--- laravel/app/database/seeds/DatabaseSeeder.php
+++ laravel/app/database/seeds/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
       public function run()
       {
             Eloquent::unguard();
-            /*$this->call('AdministradoresTableSeeder');
+            $this->call('AdministradoresTableSeeder');
             $this->call('MarketingsTableSeeder');
             $this->call('CatalogoTableSeeder');
-            $this->call('UserTableSeeder');            */
+            /*$this->call('UserTableSeeder');            */
             /*$this->call('ClientesTableSeeder');*/
             
       }
