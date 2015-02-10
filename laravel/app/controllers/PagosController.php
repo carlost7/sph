501b1cadc7071a4db62670974ed10019e458ecef
carlos <carlos@carlos>
2015-02-10 14:03:33 -0600

    changes in negocio

--- laravel/app/controllers/PagosController.php
+++ laravel/app/controllers/PagosController.php
@@ -185,7 +185,7 @@ class PagosController extends \BaseController {
       public function recibir_notificacion()
       {
             Log::info('PagosController.recibir_notificacion entrada de datos');
-            Log::error("PagosController.recibir_notificacion".print_r(Input::all()));
+            Log::error("PagosController.recibir_notificacion".print_r(Input::all(),true));
 
             if (Config::get('params.prueba_pago'))
             {
