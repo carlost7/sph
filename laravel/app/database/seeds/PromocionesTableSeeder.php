e28f64d5b3ae9019fb2231f4d71502c0f8598933
carlos <carlos@carlos>
2015-02-17 16:12:30 -0600

    agregar al db seed promociones

---
+++ laravel/app/database/seeds/PromocionesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+// Composer: "fzaninotto/faker": "v1.4.0"
+use Faker\Factory as Faker;
+
+class PromocionesTableSeeder extends Seeder {
+
+      public function run()
+      {
+            $faker = Faker::create();
+
+            $negocios = Negocio::all();
+
+            foreach ($negocios as $negocio) {
+                  $i         = rand(1, 2);                  
+                  $promocion = new Promocion([                      
+                      'nombre'             => $faker->name,
+                      'codigo'             => $faker->domainName,
+                      'descripcion'        => $faker->text(100),                      
+                      'vigencia_inicio'    => $faker->date(),
+                      'vigencia_fin'       => $faker->date(),
+                      'publicar'           => true,
+                      'imagen'             => ($i == 1) ? "http://sphellar.com/mx/wp-content/uploads/2014/11/desierto-de-liwa-nota.jpg" : "http://sphellar.com/mx/wp-content/uploads/2014/11/mexico-cenotes-cuzama.jpg",
+                      'tiempo_publicacion' => $faker->numberBetween(1, 3),
+                      'negocio_id'         => $negocio->id,
+                  ]);
+                  $negocio->promociones()->save($promocion);                   
+            }
+      }
+
+}
